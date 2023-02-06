1ebcf142f201201f4d291d93c7cb93f5f3fe61d6
zenghaifeng <zenghaifenghandsome@gmail.com>
2023-02-06 21:21:33 +0800

    theme demo

---
+++ theme/app.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hello">
+   <el-button type="primary" size="mini" @click="moren"
+        >默认主题</el-button
+      >
+      <el-button type="danger" size="mini" @click="hei">暗黑</el-button>
+      <el-color-picker
+        v-model="color"
+        show-alpha
+        @change="zidingyi"
+      ></el-color-picker> 
+  </div>
+</template>
+ 
+ 
+<script>
+import { setTheme } from "@/theme/styleAll";
+export default {
+  name: "HelloWorld",
+data(){
+return{
+color:''
+}
+},
+  mounted() {
+    this.init(); // 初始化主题
+  },
+  methods: {
+    init() {
+      setTheme("default"); // 初始化未默认主题
+    },
+    // 更改为默认主题
+    moren() {
+      setTheme("default");
+    },
+    // 更改为暗黑主题
+    hei() {
+      setTheme("dark");
+    },
+    // 更改为自定义主题
+    zidingyi() {
+      let newPrimaryColor = this.color;
+      localStorage.setItem("primaryColor", newPrimaryColor); // 将新的主题色存入本地
+      localStorage.setItem("primaryTextColor", newPrimaryColor); // 将新的主题色存入本地
+      setTheme();
+    },
+  },
+};
+</script>
+<style scoped lang="less">
+.hello {
+background:@primaryColor;
+    color: @primaryTextColor;
+}
+</style>
---
+++ theme/model.js
@@ -0,0 +1,13 @@
+// src/theme/model.js
+// 一套默认主题以及一套暗黑主题
+// 一套默认主题以及一套暗黑主题
+export const themes = {
+    default: {
+      primaryColor: 'rgba(227,38,38,1)',
+      primaryTextColor: 'rgba(74,144,226,1)',
+    },
+    dark: {
+      primaryColor: 'rgba(0,0,0,1)',
+      primaryTextColor: 'rgba(0,0,0,1)',
+    },
+  };
